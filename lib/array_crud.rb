98fcd3c5f16313b17dd303fef363ff901e025397
Ting Ting Zheng <ttzhenng@gmail.com>
2020-09-29 05:12:54 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,8 @@ def create_an_array
   return c
 end
 
-def add_element_to_end_of_array(c, element)
-  array.push(element)
+def add_element_to_end_of_array(c , element)
+  c.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
