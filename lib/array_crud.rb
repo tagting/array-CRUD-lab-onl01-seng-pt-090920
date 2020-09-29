d65c38fea395a2915aa8e2c73c2621b600e09a33
Ting Ting Zheng <ttzhenng@gmail.com>
2020-09-29 04:49:07 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-  
+  color = ["red", "orange", "yellow", "green"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  color << "blue"
 end
 
 def add_element_to_start_of_array(array, element)
