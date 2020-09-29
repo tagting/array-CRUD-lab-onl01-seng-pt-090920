7a18a19ee12ca037fcb8ef45bbb55ad6ec62a75d
Ting Ting Zheng <ttzhenng@gmail.com>
2020-09-29 05:20:47 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,36 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  c = ["red", "orange", "yellow", "green"]
+  return c
 end
 
-def add_element_to_end_of_array(array, element)
-  
+def add_element_to_end_of_array(c , element)
+  c.push(element)
 end
 
-def add_element_to_start_of_array(array, element)
-  
+def add_element_to_start_of_array(c, element)
+  c.unshift(element)
 end
 
-def remove_element_from_end_of_array(array)
-  
+def remove_element_from_end_of_array(c)
+  c.pop
 end
 
-def remove_element_from_start_of_array(array)
-  
+def remove_element_from_start_of_array(c)
+  c.shift
 end
 
-def retrieve_element_from_index(array, index_number)
-  
+def retrieve_element_from_index(c, index_number)
+  c[index_number]
 end
 
-def retrieve_first_element_from_array(array)
-  
+def retrieve_first_element_from_array(c)
+  c[0]
 end
 
-def retrieve_last_element_from_array(array)
-  
+def retrieve_last_element_from_array(c)
+  c[-1]
 end
