ad60102cea986c48fba2e7c3f285e44f677c8bcf
Ting Ting Zheng <ttzhenng@gmail.com>
2020-09-29 05:13:46 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,8 +11,8 @@ def add_element_to_end_of_array(c , element)
   c.push(element)
 end
 
-def add_element_to_start_of_array(array, element)
-  
+def add_element_to_start_of_array(c, element)
+  c.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
